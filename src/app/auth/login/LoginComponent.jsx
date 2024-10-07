9a1360e81c1cdc313ea9d378718c5440372bc378
aryansingla <singlaaryan2001@gmail.com>
2024-10-07 20:44:33 +0530

    changes

---
+++ src/app/auth/login/LoginComponent.jsx
@@ -0,0 +1,37 @@
+import {
+    Box,
+    Typography,
+  } from "@mui/material";
+  import { redirect } from "next/navigation";
+  import { getServerSession } from "next-auth";
+  import Login from "@/components/Auth/Login";
+  import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+  
+  export default async function LoginComponent() {
+    
+    const data = await getServerSession(authOptions);
+  
+     if (data?.user) {
+       redirect("/dashboard");
+     }
+    
+     return (
+        <Box
+          sx={{
+            position: "relative",
+            "&:before": {
+              content: '""',
+              background: "black",
+              backgroundSize: "400% 400%",
+              animation: "gradient 15s ease infinite",
+              position: "absolute",
+              height: "100%",
+              width: "100%",
+              opacity: "0.3",
+            },
+          }}
+        >
+          <Login/>
+        </Box>
+     );
+   }
--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,53 +1,13 @@
-import {
-    Box,
-    Typography,
-  } from "@mui/material";
-  import { redirect } from "next/navigation";
-  import { getServerSession } from "next-auth";
-  import Login from "@/components/Auth/Login";
-  import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-  
-//   export default async function Login2() {
-    
-//     const data = await getServerSession(authOptions);
-  
-//     if (data?.user) {
-//       redirect("/dashboard");
-//     }
-    
-//     return (
-//       // <Box
-//       //   sx={{
-//       //     position: "relative",
-//       //     "&:before": {
-//       //       content: '""',
-//       //       background: "black",
-//       //       backgroundSize: "400% 400%",
-//       //       animation: "gradient 15s ease infinite",
-//       //       position: "absolute",
-//       //       height: "100%",
-//       //       width: "100%",
-//       //       opacity: "0.3",
-//       //     },
-//       //   }}
-//       // >
-//       //   <Login/>
-//       // </Box>
-//       <>
-//       <Typography sx={{color:'#fff'}}>login page</Typography>
-      
-//       </>
-//     );
-//   }
+
   
   
 import React from 'react'
+import LoginComponent from "./LoginComponent"
 
 const page = () => {
   return (
     <>
-    <div style={{color:'#000'}}>Login</div>
-    <div style={{color:'#fff'}}>Login</div>
+   <LoginComponent/>
     </>
 
   )
