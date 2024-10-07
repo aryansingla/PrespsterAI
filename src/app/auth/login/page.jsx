4dade671eaafc8fb36cacef0779296e642d13f1c
aryansingla <singlaaryan2001@gmail.com>
2024-10-07 20:38:53 +0530

    changes

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,19 +1,16 @@
-// import {
-//     Box,
-//     Typography,
-//   } from "@mui/material";
-//   import { redirect } from "next/navigation";
-//   import { getServerSession } from "next-auth";
-//   import Login from "@/components/Auth/Login";
-//   import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import React from 'react'
+import {
+    Box,
+    Typography,
+  } from "@mui/material";
+  import { redirect } from "next/navigation";
+  import { getServerSession } from "next-auth";
+  import Login from "@/components/Auth/Login";
+  import { authOptions } from "@/app/api/auth/[...nextauth]/route";
   
 //   export default async function Login2() {
     
-//     const data = await getServerSession(authOptions);
-  
-//     if (data?.user) {
-//       redirect("/dashboard");
-//     }
+//     
     
 //     return (
 //       // <Box
@@ -41,13 +38,33 @@
 //   }
   
   
-import React from 'react'
 
-const page = () => {
+
+const page = async () => {
+  const data = await getServerSession(authOptions);
+  
+    if (data?.user) {
+      redirect("/dashboard");
+    }
   return (
     <>
-    <div style={{color:'#000'}}>Login</div>
-    <div style={{color:'#fff'}}>Login</div>
+    <Box
+        sx={{
+          position: "relative",
+          "&:before": {
+            content: '""',
+            background: "black",
+            backgroundSize: "400% 400%",
+            animation: "gradient 15s ease infinite",
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+            opacity: "0.3",
+          },
+        }}
+      >
+        <Login/>
+      </Box>
     </>
 
   )
